7e1ce1efc5ed319baaacc5ee5c5397081de4ed57
haoryudai0311-create <hao.ryudai0311@gmail.com>
2026-07-13 23:52:25 +0900

    Create index.tsx

---
+++ pages/index.tsx
@@ -0,0 +1,14 @@
+import Head from 'next/head';
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>SUPER CAR WIKI</title>
+      </Head>
+      <main className="min-h-screen flex items-center justify-center bg-zinc-900 text-white">
+        <h1 className="text-3xl font-bold">Supercar-Wiki　準備中…</h1>
+      </main>
+    </>
+  );
+}
